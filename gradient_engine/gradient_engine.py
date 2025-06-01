75a549a74cea3f37d41b25b6be4fe62008a7acf6
Nolan <62817066+ncm88@users.noreply.github.com>
2025-06-01 16:05:58 -0400

    they call me the tabulator

--- gradient_engine/gradient_engine.py
+++ gradient_engine/gradient_engine.py
@@ -6,14 +6,14 @@ EPS = 1e-6 #1 in a million, Baby
 class Gradient_Engine:
 
     def __init__(self, func, loss_func, quant_func, func_device, loss_func_device, quant_func_device, tensor):    
-        self.func = func
-        self.loss_func = loss_func
-        self.quant_func = quant_func
-        self.func_device = func_device
-        self.loss_func_device = loss_func_device
-        self.quant_func_device = quant_func_device
-        self.tensor = tensor
-        self.gradient = torch.zeros_like(self.tensor)
+        self.func                   = func
+        self.loss_func              = loss_func
+        self.quant_func             = quant_func
+        self.func_device            = func_device
+        self.loss_func_device       = loss_func_device
+        self.quant_func_device      = quant_func_device
+        self.tensor                 = tensor
+        self.gradient               = torch.zeros_like(self.tensor)
         
 
     def compute_gradient(self, **kwargs):
@@ -29,34 +29,30 @@ class NES_Engine(Gradient_Engine):
 
 
     def compute_gradient(self, perturbation_scale_factor, num_perturbations, vecMin=None, vecMax=None):
-        tensor = self.tensor    #Optimize for locality
-        func = self.func
-        loss_func = self.loss_func
-        quant_func = self.quant_func
-        func_device = self.func_device
-        loss_func_device = self.loss_func_device
-        quant_func_device = self.quant_func_device
+        tensor              = self.tensor    #Optimize for locality
+        func                = self.func
+        loss_func           = self.loss_func
+        quant_func          = self.quant_func
+        func_device         = self.func_device
+        loss_func_device    = self.loss_func_device
+        quant_func_device   = self.quant_func_device
+        device              = tensor.device
 
-        device = tensor.device
-
-        perturbations = generate_perturbation_vectors(num_perturbations, tensor.shape, device) #[[p11, p12, p13], [p21, p22, p23], [p31, p32, p33]]
-        last_output = func(tensor).to(loss_func_device)
+        perturbations       = generate_perturbation_vectors(num_perturbations, tensor.shape, device) #[[p11, p12, p13], [p21, p22, p23], [p31, p32, p33]]
+        last_output         = func(tensor).to(loss_func_device)
 
         if vecMin is not None and vecMax is not None:
-            
-            safe_scale = anal_clamp(tensor, perturbations, vecMin, vecMax, perturbation_scale_factor)
-
-            cand_batch = (tensor + perturbations * safe_scale).to(func_device).clamp(vecMin, vecMax) #[t1, t2, t3] + [[p11, p12, p13], [p21, p22, p23], [p31, p32, p33]] -> [[c11, c12, c13], [c21, c22, c23], [c31, c32, c33]] where cxy = t[y] + p[x,y]
+            safe_scale      = anal_clamp(tensor, perturbations, vecMin, vecMax, perturbation_scale_factor)
+            cand_batch      = (tensor + perturbations * safe_scale).to(func_device).clamp(vecMin, vecMax) #[t1, t2, t3] + [[p11, p12, p13], [p21, p22, p23], [p31, p32, p33]] -> [[c11, c12, c13], [c21, c22, c23], [c31, c32, c33]] where cxy = t[y] + p[x,y]
         
         else:
-            cand_batch = (tensor + perturbations).to(func_device)    #Clampage sorta redundant here but better safe than sorry
+            cand_batch      = (tensor + perturbations).to(func_device)
 
         if quant_func is not None:
-            cand_batch = quant_func(cand_batch.to(quant_func_device))
+            cand_batch      = quant_func(cand_batch.to(quant_func_device))
 
-        new_outputs = func(cand_batch).to(loss_func_device) 
-        
-        diffs = loss_func(new_outputs, last_output)       
-        deltas = diffs.sum(dim=1).to(tensor.dtype).view(num_perturbations, *((1,) * tensor.dim()))
+        new_outputs         = func(cand_batch).to(loss_func_device) 
+        diffs               = loss_func(new_outputs, last_output)       
+        deltas              = diffs.sum(dim=1).to(tensor.dtype).view(num_perturbations, *((1,) * tensor.dim()))
 
         return (deltas * perturbations.to(device)).sum(dim=0).to(device).view(tensor.shape)  #[d1, d2, d3] -> VecSum([[d1], [d2], [d3]] * [[p11, p12, p13], [p21, p22, p23], [p31, p32, p33]]) -> [g1, g2, g3] where gx = [dx] * [px1, px2, px3]
--- optimizer/optimizer.py
+++ optimizer/optimizer.py
@@ -7,9 +7,9 @@ from spectra.deltagrad.utils import anal_clamp
 class Optimizer:
 
     def __init__(self, func_package: tuple, device_package: tuple, tensor: torch.tensor, **kwargs):
-        self.func, self.loss_func, self.quant_func = func_package
+        self.func, self.loss_func, self.quant_func                      = func_package
         self.func_device, self.loss_func_device, self.quant_func_device = device_package
-        self.tensor = tensor.clone()
+        self.tensor                                                     = tensor.clone()
 
 
     def get_delta(self, **kwargs):
@@ -24,49 +24,47 @@ class NES_Signed_Optimizer(Optimizer):
         self.vecMax = vecMax
         super().__init__(func_package, device_package, tensor)
         self.engine = NES_Engine(
-            func=self.func, 
-            loss_func=self.loss_func, 
-            quant_func=self.quant_func, 
-            func_device=self.func_device, 
-            loss_func_device=self.loss_func_device, 
-            quant_func_device=self.quant_func_device, 
+            func                = self.func, 
+            loss_func           = self.loss_func, 
+            quant_func          = self.quant_func, 
+            func_device         = self.func_device, 
+            loss_func_device    = self.loss_func_device, 
+            quant_func_device   = self.quant_func_device, 
             tensor=self.tensor)
     
     
     #Step coefficient encodes step size and direction 
     def get_delta(self, step_coeff, num_steps, perturbation_scale_factor, num_perturbations, beta=1, acceptance_func=None):
-        quant_func = self.quant_func
+        quant_func      = self.quant_func
 
-        tensor = self.tensor
-        device = tensor.device
+        tensor          = self.tensor
+        device          = tensor.device
 
-        vecMin = self.vecMin
-        vecMax = self.vecMax    #Optimize for locality
+        vecMin          = self.vecMin
+        vecMax          = self.vecMax    #Optimize for locality
 
-        alpha = 1 - beta
+        alpha           = 1 - beta
 
-        delta = torch.zeros_like(tensor)
-        prev_step = torch.zeros_like(tensor)
+        delta           = torch.zeros_like(tensor)
+        prev_step       = torch.zeros_like(tensor)
 
-        output_delta = delta    #Tensors shallow copy by default
+        output_delta    = delta    #Tensors shallow copy by default
 
         for _ in range(num_steps): 
 
-            step = torch.sign(self.engine.compute_gradient(perturbation_scale_factor=perturbation_scale_factor, num_perturbations=num_perturbations, vecMin=self.vecMin, vecMax=self.vecMax)) 
+            step            = torch.sign(self.engine.compute_gradient(perturbation_scale_factor=perturbation_scale_factor, num_perturbations=num_perturbations, vecMin=self.vecMin, vecMax=self.vecMax)) 
 
             if vecMin is not None and vecMax is not None:
-                
-                safe_scale = anal_clamp(tensor, step, vecMin, vecMax, perturbation_scale_factor)
-                
-                step = step * safe_scale
+                safe_scale  = anal_clamp(tensor, step, vecMin, vecMax, perturbation_scale_factor)
+                step        = step * safe_scale
 
-            step = step * step_coeff * beta + prev_step * alpha
-            prev_step = step
+            step            = step * step_coeff * beta + prev_step * alpha
+            prev_step       = step
 
-            tensor += step
-            delta += step
+            tensor          += step
+            delta           += step
 
-            tensor = quant_func(tensor)
+            tensor          = quant_func(tensor)
 
             if acceptance_func is not None:
                 if acceptance_func(tensor, delta):
@@ -85,61 +83,59 @@ class NES_Optimizer(Optimizer):
         self.vecMax = vecMax
         super().__init__(func_package, device_package, tensor)
         self.engine = NES_Engine(
-            func=self.func, 
-            loss_func=self.loss_func, 
-            quant_func=self.quant_func, 
-            func_device=self.func_device, 
-            loss_func_device=self.loss_func_device, 
-            quant_func_device=self.quant_func_device, 
-            tensor=self.tensor)
+            func                = self.func, 
+            loss_func           = self.loss_func, 
+            quant_func          = self.quant_func, 
+            func_device         = self.func_device, 
+            loss_func_device    = self.loss_func_device, 
+            quant_func_device   = self.quant_func_device, 
+            tensor              = self.tensor)
     
     
     #Step coefficient encodes step size and direction 
     def get_delta(self, step_coeff, num_steps, perturbation_scale_factor, num_perturbations, beta=1, acceptance_func=None):
         quant_func = self.quant_func
 
-        tensor = self.tensor
-        device = tensor.device
+        tensor          = self.tensor
+        device          = tensor.device
 
-        vecMin = self.vecMin
-        vecMax = self.vecMax    #Optimize for locality
+        vecMin          = self.vecMin
+        vecMax          = self.vecMax    #Optimize for locality
 
-        alpha = 1 - beta
+        alpha           = 1 - beta
 
-        delta = torch.zeros_like(tensor)
-        prev_step = torch.zeros_like(tensor)
+        delta           = torch.zeros_like(tensor)
+        prev_step       = torch.zeros_like(tensor)
 
-        output_delta = delta    #Tensors shallow copy by default
+        output_delta    = delta    #Tensors shallow copy by default
 
         for _ in range(num_steps): 
 
-            step = self.engine.compute_gradient(perturbation_scale_factor=perturbation_scale_factor, num_perturbations=num_perturbations, vecMin=self.vecMin, vecMax=self.vecMax) #The step is very large so we have to normalize it
-            tensorMax, idx = torch.abs(tensor).view(-1).max(0)
-            stepMax, idy = torch.abs(step).view(-1).max(0)
+            step            = self.engine.compute_gradient(perturbation_scale_factor=perturbation_scale_factor, num_perturbations=num_perturbations, vecMin=self.vecMin, vecMax=self.vecMax) #The step is very large so we have to normalize it
+            tensorMax, idx  = torch.abs(tensor).view(-1).max(0)
+            stepMax, idy    = torch.abs(step).view(-1).max(0)
 
-            step_scaledown = (tensorMax / stepMax) 
+            step_scaledown  = (tensorMax / stepMax) 
 
-            step *= step_scaledown
+            step            *= step_scaledown
 
 
-#            step_norm = step.norm()
-#            tensor_norm = tensor.norm()
-#            step *= (tensor_norm / (step_norm + EPS))
+            #            step_norm = step.norm()
+            #            tensor_norm = tensor.norm()
+            #            step *= (tensor_norm / (step_norm + EPS))
 
 
             if vecMin is not None and vecMax is not None:
+                safe_scale  = anal_clamp(tensor, step, vecMin, vecMax, perturbation_scale_factor)
+                step        = step * safe_scale
 
-                safe_scale = anal_clamp(tensor, step, vecMin, vecMax, perturbation_scale_factor)
-                
-                step = step * safe_scale
+            step            = (step * beta + prev_step * alpha) * step_coeff
+            prev_step       = step
 
-            step = (step * beta + prev_step * alpha) * step_coeff
-            prev_step = step
+            tensor          += step
+            delta           += step
 
-            tensor += step
-            delta += step
-
-            tensor = quant_func(tensor)
+            tensor          = quant_func(tensor)
 
             if acceptance_func is not None:
                 if acceptance_func(tensor, delta):
